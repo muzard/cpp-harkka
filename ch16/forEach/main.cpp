f8a3ae96997245834406b6b3bb84820e4a2b72e0
Arttu Kokki <arttu.kokki@gmail.com>
2026-02-12 20:43:26 +0200

    finding names 2

--- ch16/forEach/main.cpp
+++ ch16/forEach/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #include <vector>
 
+template <typename T>
+bool existsEqual(std::vector<T> arr, T item)
+{
+    bool itemFound {false};
+
+    for (auto& elem : arr)
+    {
+        if (item == elem)
+        {
+            itemFound = true;
+            break;
+        }
+    }
+
+    return itemFound;
+}
+
 int main()
 {
     std::vector<std::string> names{
@@ -13,11 +30,13 @@ int main()
     std::string input {};
     std::cin >> input;
 
-    bool nameFound {false};
+    /* bool nameFound {false};
     for (std::string_view name : names)
     {
         if (name == input) nameFound = true;
-    }
+    } */
+
+    bool nameFound {existsEqual(names, input)};
 
     if (nameFound)
         std::cout << input << " was found\n";
