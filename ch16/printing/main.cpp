c9a4abf44fd62088c1abb51d97386bc07b8dab2e
Arttu Kokki <arttu.kokki@gmail.com>
2026-02-12 13:55:43 +0200

    quiz 2

--- ch16/printing/main.cpp
+++ ch16/printing/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+/*
 int main()
 {
     std::vector arr{ 4, 6, 7, 3, 8, 2, 1, 9 };
@@ -10,6 +10,20 @@ int main()
     
     for (size_t idx {0}; idx < arr.size(); ++idx)
         std::cout << arr.at(idx) << "\n";
+
+    return 0;
+} */
+void printArray(const auto& arr)
+{
+    for (size_t idx {0}; idx < arr.size(); ++idx)
+        std::cout << arr.at(idx) << "\n";
+}
+
+int main()
+{
+    std::vector arr{ 4, 6, 7, 3, 8, 2, 1, 9 };
+
+    printArray(arr); // use function template to print array
 
     return 0;
 }
